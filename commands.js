e85dacea125b619c0552e6b96539b5233508237c
HarshSharma225 <work.harshsharma.07@gmail.com>
2026-06-15 23:56:55 +0530

    practice list commands

--- commands.js
+++ commands.js
@@ -13,7 +13,19 @@ client.on('error', err => console.log('Redis Client Error', err));
 
 await client.connect();
 
+// let res = await client.set("cId",120,"123")
 // let res = await client.mSet({"FullName": "Harsh Sharma", "LastName":"Sharma"})
 
+// INSERT IN LIST
+// let res = await client.lPush("animals","lion")
+// let res = await client.rPush("animals",["monkey","zebra","Dog"])
+
+// let res = await client.lRange('animals', 0,-1);
+// let res = await client.lRange('animals', 1,2);
+
+// await client.lPush("animals","cat");
+let res = await client.blPop("animals",0);
 console.log(res)
+
+
 process.exit(0);
